3686d2a262cde88f6234de435d1bbdbcb0b4812a
Miura55 <acordion.piano@gmail.com>
2023-06-11 21:36:48 +0900

    Update sample server code

--- app.py
+++ app.py
@@ -6,8 +6,3 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
